e6ff57f98b0c88e817fb2f5b944aaff03d121751
devendra-alt <devendramulewa171@gmail.com>
2023-05-27 17:20:39 +0530

    Added guest list using guest-list-object-array and fetch

--- index.js
+++ index.js
@@ -9,3 +9,39 @@ mobileMenuIcon.addEventListener('click', () => {
 mobileMenuClose.addEventListener('click', () => {
   mobileMenu.classList.toggle('visibility');
 });
+
+const guestCard = (guest) => {
+  const guestLiCard = document.createElement('li');
+  guestLiCard.id = guest.id;
+  guestLiCard.classList.add('guest-card');
+  const guestImgEl = document.createElement('div');
+  guestImgEl.classList.add('guest-image');
+  const imgEl = document.createElement('img');
+  imgEl.src = guest.image;
+  imgEl.id = guest.id;
+  guestImgEl.appendChild(imgEl);
+  const guestDetailsEl = document.createElement('div');
+  guestDetailsEl.classList.add('guest-details');
+  guestDetailsEl.innerHTML += `
+    <p class="guest-name">${guest.name}</p>
+    <p class="guest-designation">${guest.designation}</p>
+    <p class="guest-brif">${guest.briefSummary}</p>
+  `;
+  guestLiCard.appendChild(guestImgEl);
+  guestLiCard.appendChild(guestDetailsEl);
+  return guestLiCard;
+};
+
+const loadFeaturedGuest = () => {
+  fetch('featured-guests.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const guestsList = data.featuredGuests;
+      const guestListParent = document.querySelector('.guests-list');
+      guestsList.forEach((element) => {
+        guestListParent.appendChild(guestCard(element));
+      });
+    });
+};
+
+loadFeaturedGuest();
